fbff3cd3b3393f9597baa91110986710d2f3b00a
Xtal Osc <xtalosc@gmx.com>
2017-05-10 11:41:52 -0500

    added fake api

---
+++ src/api/authorData.js
@@ -0,0 +1,20 @@
+module.exports = {
+	authors:
+	[
+		{
+			id: 'samer-buna',
+			firstName: 'Samer',
+			lastName: 'Buna'
+		},
+		{
+			id: 'cory-house',
+			firstName: 'Cory',
+			lastName: 'House'
+		},
+		{
+			id: 'hendrik-swanepoel',
+			firstName: 'Hendrik',
+			lastName: 'Swanepoel'
+		}
+	]//end author array
+};//end module.exports
